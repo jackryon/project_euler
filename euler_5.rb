42654e3fa074e4bedc4ff8253eac160f56881aa9
Jack Ryon <jackryon@gmail.com>
2014-04-25 13:25:04 -0400

    cleanups

--- euler_5.rb
+++ euler_5.rb
@@ -2,18 +2,15 @@
 # What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
 
 max_factor = 20
-possible_factors = max_factor.downto(2).to_a
+possible_factors = max_factor.downto(10).to_a
 upper_limit = possible_factors.inject(1) {|product, i| product * i}
-
 test_num = 2520
+
 smallest_multiple = catch(:sm_mlt) do
   while test_num < upper_limit do
     possible_factors.each do |pf|
-      if test_num % pf != 0
-        break
-      elsif pf == possible_factors.last
-        throw :sm_mlt, test_num
-      end
+      break if test_num % pf != 0
+      throw :sm_mlt, test_num if pf == possible_factors.last
     end
     test_num += 2520
   end
