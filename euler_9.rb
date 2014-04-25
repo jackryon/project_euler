0769399a9a388f45223bfaa657ada8f6ff97242f
Jack Ryon <jackryon@gmail.com>
2014-04-25 15:44:47 -0400

    #9 solution

---
+++ euler_9.rb
@@ -0,0 +1,21 @@
+=begin
+A Pythagorean triplet is a set of three natural numbers, a < b < c, for which, a^2 + b^2 = c^2
+For example, 3^2 + 4^2 = 9 + 16 = 25 = 5^2.
+There exists exactly one Pythagorean triplet for which a + b + c = 1000.
+Find the product abc. 
+=end
+
+max_count = 500
+triples = []
+
+solution = catch(:trpl) do
+  (1..max_count).each do |a|
+    (1..max_count).each do |b|
+      (1..max_count).each do |c|
+        throw(:trpl, a * b * c) if (a + b + c == 1000) && (a ** 2 + b ** 2 == c ** 2)
+      end
+    end
+  end
+end
+
+puts solution
