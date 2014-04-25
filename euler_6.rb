a0498e517cf940759156f78ceb6a5e0472d4f133
Jack Ryon <jackryon@gmail.com>
2014-04-25 13:42:20 -0400

    six was way easy.. sup with that?

---
+++ euler_6.rb
@@ -0,0 +1,17 @@
+=begin
+
+The sum of the squares of the first ten natural numbers is 1^2 + 2^2 + ... + 10^2 = 385
+The square of the sum of the first ten natural numbers is (1 + 2 + ... + 10)^2 = 55^2 = 3025
+Hence the difference between the sum of the squares of the first ten natural numbers and the square of the sum is 3025 − 385 = 2640.
+Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum.
+
+=end
+
+max = 100
+
+sum_of_squares = (1..max).inject(0) {|sum, i| sum + (i ** 2)}
+square_of_sums = (1..max).inject(0) {|sum, i| sum + i} ** 2
+
+puts "sum_of_squares is #{sum_of_squares}"
+puts "square_of_sums is #{square_of_sums}"
+puts "difference is #{square_of_sums - sum_of_squares}"
